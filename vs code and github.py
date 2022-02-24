707aa2bc75c1d87a09571065d09656ab01aca4dd
Zhiwei Zhang <zhiwei.zhang3@cn.bosch.com>
2022-02-24 10:05:22 +0800

    5 change message

--- vs code and github.py
+++ vs code and github.py
@@ -1,4 +1,4 @@
 print('this is an example to connect the github or git and vs code')
 print('the second change')
 print('the third change')
-print('this is the 4 change')
+print('this is the 5 change')
